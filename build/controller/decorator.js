69932bc049d2522904538604c54b5c103b451fd0
guonan <838093891@qq.com>
2021-11-07 23:26:39 +0800

    不同请求01

--- build/controller/decorator.js
+++ build/controller/decorator.js
@@ -2,12 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 exports.router = express_1.Router();
+var Method;
+(function (Method) {
+    Method["get"] = "get";
+    Method["post"] = "post";
+})(Method || (Method = {}));
 function controller(target) {
     for (var key in target.prototype) {
         var path = Reflect.getMetadata('path', target.prototype, key);
+        var method = Reflect.getMetadata('method', target.prototype, key);
         var handler = target.prototype[key];
-        if (path) {
-            exports.router.get(path, handler);
+        if (path && method && handler) {
+            exports.router[method](path, handler);
         }
     }
 }
@@ -15,6 +21,14 @@ exports.controller = controller;
 function get(path) {
     return function (target, key) {
         Reflect.defineMetadata('path', path, target, key);
+        Reflect.defineMetadata('method', 'get', target, key);
     };
 }
 exports.get = get;
+function post(path) {
+    return function (target, key) {
+        Reflect.defineMetadata('path', path, target, key);
+        Reflect.defineMetadata('method', 'post', target, key);
+    };
+}
+exports.post = post;
